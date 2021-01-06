28a8c8539c56327a110432a1e27d66bdca588b55
pebjack <pk.jackowski@gmail.com>
2021-01-06 20:06:54 +0100

    Initial commit

---
+++ src/Gwiazda.java
@@ -0,0 +1,7 @@
+public class Gwiazda {
+    private String nazwa, nazwaKatalogowa, gwiazdozbior, polkula;
+    private double deklinacja, rekascensja, obsMagnitudo, absMagnitudo, odleglosc, temperatura, masa;
+
+    public Gwiazda() {
+    }
+}
