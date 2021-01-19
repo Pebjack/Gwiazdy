fdcc0d8eb9122b840125de27f14a161a4e488cbc
pebjack <pk.jackowski@gmail.com>
2021-01-19 23:28:55 +0100

    Day 3 - Progress on GUI work.

--- src/GUI.java
+++ src/GUI.java
@@ -1,16 +1,18 @@
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import java.util.ArrayList;
+import javax.swing.table.*;
+import java.awt.*;
+import java.util.*;
 
 public class GUI {
 
     public static JTable wyswietlTabele(ArrayList<Gwiazda> gwiazdy){
-        String[] kolumny = {"Nazwa",
+        String[] kolumny = {
+                "Nazwa",
                 "Nazwa Katalogowa",
                 "Deklinacja",
                 "Rektascensja",
-                "Obserwowalna wielkość gwiazdowa",
-                "Absolutna wielkość gwiazdowa",
+                "Obserwowalne Magnitudo",
+                "Absolutne Magnitudo",
                 "Odległość",
                 "Gwiazdozbiór",
                 "Półkula",
@@ -18,8 +20,10 @@ public class GUI {
                 "Masa"
         };
         DefaultTableModel modelTablicy = new DefaultTableModel(kolumny, 0);
-        modelTablicy.setColumnIdentifiers(kolumny);
         JTable tabelaGwiazd = new JTable(modelTablicy);
+        tabelaGwiazd.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+        tabelaGwiazd.setOpaque(true);
+
         for(int i = 0; i < gwiazdy.size(); i++){
             Object[] dane = {
                     gwiazdy.get(i).getName(),
@@ -38,29 +42,57 @@ public class GUI {
 
         }
 
-
         return tabelaGwiazd;
     }
 
 
 
-    public static void addStar(){
-
-    }
-
     public static void main(String[] args){
         ArrayList<Gwiazda> Gwiazdy = new ArrayList<>();
-        JButton przycisk = new JButton("Dodaj gwiazdę");
+        //new Color(20,43,66)
+        Dimension button = new Dimension(200,50);
 
-        JFrame okno = new JFrame("Katalog Gwiazdowy");
-        okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        okno.setSize(1280,700);
+        JFrame oknoGlowne = new JFrame("Katalog Gwiazd");
+        oknoGlowne.setLayout(null);
+        oknoGlowne.setBackground(new Color(20,43,66));
+        oknoGlowne.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        oknoGlowne.setResizable(false);
+        oknoGlowne.setPreferredSize(new Dimension(1060,740));
 
-        Gwiazdy.add(new Gwiazda("ABC123","60.90.35.15","15.35.20",10.00, 17, "Centauri", "PD", 2000000, 10));
+        JMenuBar menuGlowne = new JMenuBar();
+        menuGlowne.setOpaque(false);
+        menuGlowne.setPreferredSize(new Dimension(1060,20));
 
-        okno.add(przycisk);
-        okno.add(wyswietlTabele(Gwiazdy));
-        okno.setVisible(true);
+        JScrollPane spisGwiazd = new JScrollPane(wyswietlTabele(Gwiazdy));
+        spisGwiazd.setOpaque(true);
+        spisGwiazd.getViewport().setBackground(new Color(20,43,66));
+        spisGwiazd.setBorder(BorderFactory.createEtchedBorder(new Color(43,91,140),new Color(31,66,102)));
+        spisGwiazd.setBounds(0,0,1060,396);
+
+        JPanel functionPanel = new JPanel();
+        functionPanel.setOpaque(true);
+        functionPanel.setBackground(new Color(20,43,66));
+        functionPanel.setBorder(BorderFactory.createEtchedBorder(new Color(43,91,140),new Color(31,66,102)));
+        functionPanel.setBounds(660,396,400,296);
+
+        JButton dodajGwiazde = new JButton("Dodaj gwiazdę");
+        dodajGwiazde.setPreferredSize(button);
+        functionPanel.add(dodajGwiazde);
+
+        JPanel dataPanel = new JPanel();
+        dataPanel.setOpaque(true);
+        dataPanel.setBackground(new Color(20,43,66));
+        dataPanel.setBorder(BorderFactory.createEtchedBorder(new Color(43,91,140),new Color(31,66,102)));
+        dataPanel.setBounds(0,396,660,296);
+
+
+        oknoGlowne.setJMenuBar(menuGlowne);
+        oknoGlowne.getContentPane().add(spisGwiazd);
+        oknoGlowne.getContentPane().add(functionPanel);
+        oknoGlowne.getContentPane().add(dataPanel);
+        oknoGlowne.pack();
+        oknoGlowne.setVisible(true);
+
 
     }
 
