76647bae0b6a7a1cfde6c92cd301c87405d832db
pebjack <pk.jackowski@gmail.com>
2021-01-19 01:36:17 +0100

    Day 2 - problems with gui

--- src/GUI.java
+++ src/GUI.java
@@ -1,15 +1,68 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.util.ArrayList;
 
 public class GUI {
 
+    public static JTable wyswietlTabele(ArrayList<Gwiazda> gwiazdy){
+        String[] kolumny = {"Nazwa",
+                "Nazwa Katalogowa",
+                "Deklinacja",
+                "Rektascensja",
+                "Obserwowalna wielkość gwiazdowa",
+                "Absolutna wielkość gwiazdowa",
+                "Odległość",
+                "Gwiazdozbiór",
+                "Półkula",
+                "Temperatura",
+                "Masa"
+        };
+        DefaultTableModel modelTablicy = new DefaultTableModel(kolumny, 0);
+        modelTablicy.setColumnIdentifiers(kolumny);
+        JTable tabelaGwiazd = new JTable(modelTablicy);
+        for(int i = 0; i < gwiazdy.size(); i++){
+            Object[] dane = {
+                    gwiazdy.get(i).getName(),
+                    gwiazdy.get(i).getCatName(),
+                    gwiazdy.get(i).getDeclin(),
+                    gwiazdy.get(i).getRightAscen(),
+                    gwiazdy.get(i).getObsMagnitudo(),
+                    gwiazdy.get(i).getAbsMagnitudo(),
+                    gwiazdy.get(i).getDist(),
+                    gwiazdy.get(i).getConstel(),
+                    gwiazdy.get(i).getHemisph(),
+                    gwiazdy.get(i).getTemp(),
+                    gwiazdy.get(i).getMass(),
+            };
+            modelTablicy.addRow(dane);
+
+        }
+
+
+        return tabelaGwiazd;
+    }
+
+
+
+    public static void addStar(){
+
+    }
+
     public static void main(String[] args){
+        ArrayList<Gwiazda> Gwiazdy = new ArrayList<>();
+        JButton przycisk = new JButton("Dodaj gwiazdę");
+
         JFrame okno = new JFrame("Katalog Gwiazdowy");
         okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         okno.setSize(1280,700);
-        JButton przycisk = new JButton("Naciś to");
-        okno.getContentPane().add(przycisk);
 
+        Gwiazdy.add(new Gwiazda("ABC123","60.90.35.15","15.35.20",10.00, 17, "Centauri", "PD", 2000000, 10));
+
+        okno.add(przycisk);
+        okno.add(wyswietlTabele(Gwiazdy));
         okno.setVisible(true);
+
     }
+
 
 }
--- src/Gwiazda.java
+++ src/Gwiazda.java
@@ -1,37 +1,55 @@
-public class Gwiazda {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Gwiazda implements Serializable {
     // pola klasy
-    private String name, catName, constel, hemisph;
-    private double declin, rightAscen, obsMagnitudo, absMagnitudo, dist, temp, mass;
+    private String name, catalogName, constel, hemisph, declin, rightAscen;
+    private double obsMagnitudo, absMagnitudo, dist, temp, mass;
 
     // gettery stringów
     public String getName() {return name;}
-    public String getCatName() {return catName;}
+    public String getCatName() {return catalogName;}
     public String getConstel() {return constel;}
     public String getHemisph() {return hemisph;}
+    public String getDeclin() {return declin;}
+    public String getRightAscen() {return rightAscen;}
 
     // settery stringów
     public void setName(String name) {this.name = name;}
-    public void setCatName(String catName) {this.catName = catName;}
+    public void setCatName(String catName) {this.catalogName = catName;}
     public void setConstel(String constel) {this.constel = constel;}
     public void setHemisph(String hemisph) {this.hemisph = hemisph;}
+    public void setDeclin(String declin) {this.declin = declin;}
+    public void setRightAscen(String rightAscen) {this.rightAscen = rightAscen;}
 
-    //gettery doubli
-    public double getDeclin() {return declin;}
-    public double getRightAscen() {return rightAscen;}
+    // gettery doubli
+
     public double getObsMagnitudo() {return obsMagnitudo;}
     public double getAbsMagnitudo() {return absMagnitudo;}
     public double getDist() {return dist;}
     public double getTemp() {return temp;}
     public double getMass() {return mass;}
 
-    //settery doubli
-    public void setDeclin(double declin) {this.declin = declin;}
-    public void setRightAscen(double rightAscen) {this.rightAscen = rightAscen;}
+    // settery doubli
+
     public void setObsMagnitudo(double obsMagnitudo) {this.obsMagnitudo = obsMagnitudo;}
     public void setAbsMagnitudo(double absMagnitudo) {this.absMagnitudo = absMagnitudo;}
     public void setDist(double dist) {this.dist = dist;}
     public void setTemp(double temp) {this.temp = temp;}
     public void setMass(double mass) {this.mass = mass;}
 
+    public Gwiazda(String nam, String decl, String rasc, double omag, double dst, String con, String hms, double tmp, double mss){
+        this.name = nam;
+        this.constel = con;
+        this.catalogName = (char)216 + ' ' + this.constel;
+        this.declin = decl;
+        this.rightAscen = rasc;
+        this.obsMagnitudo = omag;
+        this.dist = dst;
+        this.absMagnitudo = this.obsMagnitudo - 5 * Math.log10(this.dist) + 5;
+        this.hemisph = hms;
+        this.temp = tmp;
+        this.mass = mss;
+    }
 
 }
